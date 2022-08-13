7f6da3820f7ce8f5a8c9cb4b303cae4b19b91f81
AlifurkanKaraman <82064494+AlifurkanKaraman@users.noreply.github.com>
2022-08-13 09:00:34 +0300

    Refresh added

--- src/screens/post/NetworkScreen.tsx
+++ src/screens/post/NetworkScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { ActivityIndicator, Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Dimensions, FlatList, RefreshControl, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import PostCard from '../../components/PostCard';
 import { PostService } from '../../services/postService';
@@ -12,17 +12,18 @@ const NetworkScreen: React.FC<any> = ({ navigation }) => {
     const [posts, setPosts] = React.useState<PostModel[]>([])
     const [total, setTotal] = React.useState<number>(Infinity)
     const [loading, setLoading] = React.useState(false)
+    const [refreshing, setRefreshing] = React.useState(false);
 
     useEffect(() => {
         getMorePosts()
     }, [])
 
 
-    const getMorePosts = () => {
+    const getMorePosts = async () => {
         if(posts.length >= total) return
         setLoading(true)
-        postService.getAllPosts(posts.length).then(res => {
-            posts.push(...res.data.data)
+        await postService.getAllPosts(posts.length).then(res => {
+            posts.push(...res.data.data?.reverse())
             setPosts(posts)
             setLoading(false)
             setTotal(res.data.total)
@@ -30,9 +31,21 @@ const NetworkScreen: React.FC<any> = ({ navigation }) => {
     }
     console.log(posts.keys())
 
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        setPosts([])
+        getMorePosts().then(x => {
+            setRefreshing(false)
+        })
+      }, []);
+
     return (
         <SafeAreaView style={styles.root}>
             <FlatList
+            refreshControl={<RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+              />}
                 data={posts}
                 keyExtractor={(item) => item._id}
                 renderItem={({ item }) => <PostCard post={item} />}
